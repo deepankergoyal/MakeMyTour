dff91c83dee69ca189bfe3bfcf87d4fa0c4f63bc
deepankergoyal <deepankergoyal1@gmail.com>
2024-02-14 18:43:10 -0500

    jwt

--- app.js
+++ app.js
@@ -11,11 +11,21 @@ app.use(express.json()); // app.use is used to a midddleware in the middleware s
 
 app.use(morgan('dev'));
 
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  // console.log(req.headers);
+  next();
+});
+
 // app.get('/', (req, res) => {
 //   res.send('Hello, world!'); // Sends "Hello, world!" as the response when someone accesses the root route
 // });
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
 
 module.exports = app;
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const tourController = require('./../controllers/tourController');
 const router = express.Router();
+const authController = require('./../controllers/authController');
 
 router
   .route('/')
-  .get(tourController.getAllTours)
+  .get(authController.protect, tourController.getAllTours)
   .post(tourController.createTour);
 
 router
